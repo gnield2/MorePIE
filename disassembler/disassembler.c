e5cbd63bdbe895fb55732f41bc6c4a61ac5d7d8b
gnield2 <97625255+gnield2@users.noreply.github.com>
2023-09-27 15:26:22 -0400

    Added main()

--- disassembler/disassembler.c
+++ disassembler/disassembler.c
@@ -125,3 +125,31 @@ int Disassemble6502p(unisgned char* buffer, int pc)
         /* Raul does opcode $be to opcode $fe */
     }
 }
+
+int main(int argc, char* argv[])
+{
+    FILE *f = fopen(argv[1], "r");
+    if (f == NULL)
+    {
+        fprintf(stderr, "Error: Failed to open %s\n", argv[1]);
+        exit(1);
+    }
+
+    fseek(f, 0L, SEEK_END);
+    int fsize = ftell(f);
+    fseek(f, 0L, SEEK_SET);
+
+    unsigned char* buffer = malloc(fsize);
+
+    fread(buffer, fsize, 1, f);
+    fclose(f);
+
+    int pc = 0;
+
+    while (pc < fsize)
+    {
+        pc += Disassemble6502p(buffer, pc);
+    }
+
+    return 0;
+}
