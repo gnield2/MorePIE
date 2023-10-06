97a006c82e1878699a3a4beed5ca38052cc737f8
Jeriah Muleya <ellocoburrito56@icloud.com>
2023-10-06 16:36:05 -0400

    do not merge

--- src/6502emulator.c
+++ src/6502emulator.c
@@ -700,7 +700,7 @@ int main() {
     printf("dummy main funciton\n");
     State6502 test;
     test.stack[1] = 0x99;
-    test.stack[2] = 0x2000;
+    test.stack[2] = 2000;
     test.stack[3] = 0xFF;
     test.pc = 1;
     Emulate6502(&test);
